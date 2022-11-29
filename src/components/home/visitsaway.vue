cbde3bcc31fea7949ea987dcc3b01c87259d3d59
mastercreator001 <melon_man2000@hotmail.com>
2022-11-29 21:42:41 +0000

    Update visitsaway.vue

--- src/components/home/visitsaway.vue
+++ src/components/home/visitsaway.vue
@@ -1,50 +1,49 @@
 <template>
 <div class="cofe-container cofe-white cofe-margin cofe-padding-large">
-    <div class="cofe-center">
-      <h3>Visits Away</h3>
-      <h5>Out of School, <span class="cofe-opacity">April 7, 1985</span></h5>
-    </div>
-
-    <div class="cofe-justify">
-      <img src="./cofe.Images/c of e miss forer 1981 and undated/20200509_093057.jpg" alt="visits away" style="width:100%" class="cofe-padding-16">
-      <p><strong>Did you miss!</strong> The legendary breaks from school were more of a holiday for the students and a nightmare of the the teachers, imagine looking after 30, 40 ,50 or even 60 kids who aren't yours.</p>
-      <p>Fortuntely school trips were unifromed early in the schols history with the 1st years or year ones attending Singleton Open Air Museum. Well actually now called the Weald and Downland Open Air Experience these days, "Check out the link to visit". </p>
-  <br>
-  <br> 
-  <a href="https://www.wealddown.co.uk">
-  <img alt="wealdand down logo" src="./cofe.Images/Weald-Downland-Living-Museum-logo (1).svg" width="100" height="100">
-</a>
-
-      <p class="cofe-left"><button class="cofe-button cofe-white cofe-border" onclick="likeFunction(this)"><b><i class="fa fa-thumbs-up"></i> Like</b></button></p>
-      <p class="cofe-right"><button class="cofe-button cofe-black" onclick="myFunction('demo3')"><b>Replies &nbsp;</b> <span class="cofe-tag cofe-white">3</span></button></p>
-      <p class="cofe-clear"></p>
-      
-      <!-- Example of comment field -->
-      <div id="" style="display:none">
-        <hr>
-        <div class="cofe-row cofe-margin-bottom">
-          <div class="cofe-col l2 m3">
-            
-          </div>
+        <div class="cofe-center">
+          <h3>Visits Away</h3>
+          <h5>Out of School, <span class="cofe-opacity">April 7, 1985</span></h5>
         </div>
-        <div class="cofe-row cofe-margin-bottom">
-          <div class="cofe-col l2 m3">
-            <img src="/w3images/boy.jpg" style="width:90px;">
-          </div>
-          <div class="cofe-col l10 m9">
-            <h4><span class="cofe-opacity cofe-medium"></span></h4>
-            <p></p>
-          </div>
-        </div>
-        <div class="cofe-row cofe-margin-bottom">
-          <div class="cofe-col l2 m3">
-            <img src="" style="width:90px;">
-          </div>
-          <div class="cofe-col l10 m9">
-            
+        <div class="cofe-justify">
+          <img src="./cofe.Images/c of e miss forer 1981 and undated/20200509_093057.jpg" alt="visits away" style="width:100%" class="cofe-padding-16">
+          <p><strong>Did you miss!</strong> The legendary breaks from school were more of a holiday for the students and a nightmare of the the teachers, imagine looking after 30, 40 ,50 or even 60 kids who aren't yours.</p>
+          <p>Fortuntely school trips were unifromed early in the schols history with the 1st years or year ones attending Singleton Open Air Museum. Well actually now called the Weald and Downland Open Air Experience these days, "Check out the link to visit". </p>
+      <br>
+      <br> 
+      <a href="https://www.wealddown.co.uk">
+      <img alt="wealdand down logo" src="./cofe.Images/Weald-Downland-Living-Museum-logo (1).svg" width="100" height="100">
+    </a>
+
+          <p class="cofe-left"><button class="cofe-button cofe-white cofe-border" onclick="likeFunction(this)"><b><i class="fa fa-thumbs-up"></i> Like</b></button></p>
+          <p class="cofe-right"><button class="cofe-button cofe-black" onclick="myFunction('demo3')"><b>Replies &nbsp;</b> <span class="cofe-tag cofe-white">3</span></button></p>
+          <p class="cofe-clear"></p>
+          
+          <!-- Example of comment field -->
+          <div id="" style="display:none">
+            <hr>
+            <div class="cofe-row cofe-margin-bottom">
+              <div class="cofe-col l2 m3">
+                
+              </div>
+            </div>
+            <div class="cofe-row cofe-margin-bottom">
+              <div class="cofe-col l2 m3">
+                <img src="/w3images/boy.jpg" style="width:90px;">
+              </div>
+              <div class="cofe-col l10 m9">
+                <h4><span class="cofe-opacity cofe-medium"></span></h4>
+                <p></p>
+              </div>
+            </div>
+            <div class="cofe-row cofe-margin-bottom">
+              <div class="cofe-col l2 m3">
+                <img src="" style="width:90px;">
+              </div>
+              <div class="cofe-col l10 m9">
+                
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
 </template>
